e0dbedc82e31e5bb559fc0285f032f9c65306504
diwasbk <diwasbk903@gmail.com>
2025-12-18 01:08:31 +0545

    Create a Profile got Logged-In User

---
+++ controllers/profileController.js
@@ -0,0 +1,49 @@
+import profileModel from "../models/profileModel.js";
+import userModel from "../models/userModel.js";
+
+class ProfileController {
+    // Create a new profile for the logged-in user
+    createProfile = async (req, res) => {
+        try {
+            const { profileImageUrl, skills } = req.body
+
+            // Check if the user already has a profile
+            const profileExist = await profileModel.findOne({ userId: req.user.userId })
+
+            // If profile already exists, prevent duplicate creation
+            if (profileExist) {
+                return res.status(400).send({
+                    message: "Profile already exist!",
+                    success: false
+                })
+            }
+
+            // Create a new profile in the database
+            const createdProfile = await profileModel.create({
+                userId: req.user.userId,
+                profileImageUrl: profileImageUrl,
+                skills: skills
+            })
+
+            // Update user document to store the profile ID
+            await userModel.findOneAndUpdate(
+                { _id: req.user.userId },
+                { $set: { profile: createdProfile._id } }
+            )
+
+            res.status(201).send({
+                message: "Profile created successfully!",
+                result: createdProfile,
+                success: true
+            })
+
+        } catch (err) {
+            console.log(err)
+            res.status(500).send({
+                message: err.message ? `Internal server error: ${err.message}` : "Internal server error."
+            })
+        }
+    }
+}
+
+export default ProfileController;
