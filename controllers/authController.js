7af69dc7bd834f58c47c2058ffefda3705da5c65
diwasbk <diwasbk903@gmail.com>
2025-12-18 00:09:01 +0545

    User Login

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,6 +44,58 @@ class AuthController {
             })
         }
     }
+
+    // Login User
+    loginUser = async (req, res) => {
+        const { email, password } = req.body
+        try {
+            // Check if user exist with the provided email or not
+            const userExist = await userModel.findOne({ email: email })
+            if (!userExist) {
+                return res.status(401).send({
+                    message: "Invalid email or password",
+                    success: false
+                })
+            }
+
+            // Check password
+            const isPasswordMatch = await bcrypt.compare(password, userExist.password)
+            if (!isPasswordMatch) {
+                return res.status(401).send({
+                    message: "Invalid email or password.",
+                    success: false
+                })
+            }
+
+            // Generate Token
+            const payload = {
+                userId: userExist._id,
+                email: userExist.email,
+                role: userExist.role
+            }
+            const token = generateToken(payload)
+
+            // Set the token in cookie
+            res.cookie("token", token, {
+                httpOnly: true,
+                maxAge: 3000 * 1000,
+                sameSite: "lax",
+                secure: false
+            })
+
+            res.status(200).send({
+                message: `Logged in successfully!`,
+                success: true
+            })
+
+        } catch (err) {
+            console.log(err)
+            res.status(500).send({
+                message: err.message ? `Internal server error: ${err.message}` : "Internal server error.",
+                success: false
+            })
+        }
+    }
 }
 
 export default AuthController;
