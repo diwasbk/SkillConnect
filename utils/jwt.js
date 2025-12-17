635d7f4d9bf89a5ba626128119cfcdc8998d4bf8
diwasbk <diwasbk903@gmail.com>
2025-12-17 23:47:51 +0545

    Verify Token with jwtAuthMiddleware

--- utils/jwt.js
+++ utils/jwt.js
@@ -5,4 +5,23 @@ const generateToken = (payload) => {
     return jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: 3000 })
 }
 
-export { generateToken }
+// Auth Middleware
+const jwtAuthMiddleware = (req, res, next) => {
+    // Get token from cookie
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).send({
+            message: "Unauthorized: No token provided",
+            success: false
+        })
+    }
+    // Verify token
+    const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    // Attach user info to the request object
+    req.user = data;
+    // Pass to the next phase
+    next();
+};
+
+export { generateToken, jwtAuthMiddleware }
