c1b2321b75192836a8c5fda3a56cfcfc912fc5f2
diwasbk <diwasbk903@gmail.com>
2025-12-30 20:20:58 +0545

    Get All Reviews And Ratings Received By a Specific User

--- controllers/reviewAndRatingController.js
+++ controllers/reviewAndRatingController.js
@@ -74,6 +74,28 @@ class ReviewAndRatingController {
             })
         }
     }
+
+    // Get All Reviews And Ratings Received By a Specific User
+    getAllReviewsAndRatingsReceived = async (req, res) => {
+        try {
+            // Fetch all reviews and ratings where the user is the receiver (toUser)
+            const allReviewsAndRatingsReceived = await reviewAndRatingModel.find({ toUser: req.params.toUserId })
+
+            // Send success response with fetched reviews and ratings
+            res.status(200).send({
+                message: "All ReviewsAndRatings received.",
+                result: allReviewsAndRatingsReceived,
+                success: true
+            })
+
+        } catch (err) {
+            console.log(err)
+            res.status(500).send({
+                message: err.message ? `Internal server error: ${err.message}` : "Internal server error.",
+                success: false
+            })
+        }
+    }
 }
 
 export default ReviewAndRatingController;
--- routes/reviewAndRatingRoute.js
+++ routes/reviewAndRatingRoute.js
@@ -7,5 +7,6 @@ const reviewAndRatingController = new ReviewAndRatingController
 reviewRatingRouter.get("/", reviewAndRatingController.getAllReviewsAndRatings)
 reviewRatingRouter.get("/:reviewRatingId", reviewAndRatingController.getReviewAndRatingById)
 reviewRatingRouter.get("/given/:fromUserId", reviewAndRatingController.getAllReviewsAndRatingsGiven)
+reviewRatingRouter.get("/received/:toUserId", reviewAndRatingController.getAllReviewsAndRatingsReceived)
 
 export default reviewRatingRouter;
